da7cd0cb8a52dcf80227914a45db79053101749a
alannagibator228 <safaraliev.ad@phystech.edu>
2023-09-17 04:10:20 +0300

    speeded sort (good added struct)

--- sorting.cpp
+++ sorting.cpp
@@ -1,48 +1,51 @@
 #include <stdio.h>
 #include <ctype.h>   
 #include <stdlib.h>
-#include <math.h>   
+#include <math.h>
+#include <assert.h>
 #include "create_array.h"
 #include "sorting.h"
 
-int sor_cmp(const void* str1, const void* str2)
+// int sor_cmp(const void* str1, const void* str2)
+// {
+//     char* str_1 = *(char**) str1;
+//     char* str_2 = *(char**) str2;
+
+//     return str_cmp(str_1, str_2);
+// }
+
+// int sor_cmp_rhyme(const void* str1, const void* str2)
+// {
+//     char* str_1 = *(char**) str1;
+//     char* str_2 = *(char**) str2;
+
+//     return str_cmp_rhyme(str_1, str_2);
+// }
+
+int str_cmp(ParsedText* Text, const int num_str1, const int num_str2)
 {
-    char* str_1 = *(char**) str1;
-    char* str_2 = *(char**) str2;
+    assert(Text != NULL);     //TODO assert
+    assert(num_str1 >= 0);
+    assert(num_str2 >= 0);
 
-    return str_cmp(str_1, str_2);
-}
-
-int sor_cmp_rhyme(const void* str1, const void* str2)
-{
-    char* str_1 = *(char**) str1;
-    char* str_2 = *(char**) str2;
-
-    return str_cmp_rhyme(str_1, str_2);
-}
-
-int str_cmp(const char* str1, const char* str2)
-{
-    int len_str1 = str_len(str1);
-    int len_str2 = str_len(str2);
-    int len = fmin(len_str1, len_str2);
+    int len = fmin(Text->len_str[num_str1], Text->len_str[num_str2]);
 
     int symbol_num_1 = 0;
     int symbol_num_2 = 0;
 
     while (symbol_num_1 < len && symbol_num_2 < len) 
     {
-        if(!isalpha(str1[symbol_num_1]))
+        if(!isalpha(Text->array_of_pointers[num_str1][symbol_num_1]))
         {
             symbol_num_1++;
         }
-        else if(!isalpha(str2[symbol_num_2]))
+        else if(!isalpha(Text->array_of_pointers[num_str2][symbol_num_2]))
         {
             symbol_num_2++;
         }
-        else if(str1[symbol_num_1] != str2[symbol_num_2])
+        else if(Text->array_of_pointers[num_str1][symbol_num_1] != Text->array_of_pointers[num_str2][symbol_num_2])
         {
-            return (str1[symbol_num_1] - str2[symbol_num_2]);
+            return (Text->array_of_pointers[num_str1][symbol_num_1] - Text->array_of_pointers[num_str2][symbol_num_2]);
         }
         else
         {
@@ -50,31 +53,33 @@ int str_cmp(const char* str1, const char* str2)
         symbol_num_2++;
         }
     }
-    return (len_str1 - len_str2);
+    return (Text->len_str[num_str1] - Text->len_str[num_str2]);
 }
 
-int str_cmp_rhyme(const char* str1, const char* str2)
+int str_cmp_rhyme(ParsedText* Text, const int num_str1, const int num_str2)
 {
-    int len_str1 = str_len(str1);                     /////TODO лучше вообще записать все длины всех строчек
-    int len_str2 = str_len(str2);
-    int len = fmin(len_str1, len_str2);
+    assert(Text != NULL);     //TODO assert
+    assert(num_str1 >= 0);
+    assert(num_str2 >= 0);
 
-    int symbol_num_1 = len_str1;
-    int symbol_num_2 = len_str2;
+    int len = fmin(Text->len_str[num_str1], Text->len_str[num_str2]);
+
+    int symbol_num_1 = Text->len_str[num_str1];
+    int symbol_num_2 = Text->len_str[num_str2];
 
     while (symbol_num_1 > 0 && symbol_num_2 > 0)
     {
-        if(!isalpha(str1[symbol_num_1]))
+        if(!isalpha(Text->array_of_pointers[num_str1][symbol_num_1]))
         {
             symbol_num_1--;
         }
-        else if(!isalpha(str2[symbol_num_2]))
+        else if(!isalpha(Text->array_of_pointers[num_str2][symbol_num_2]))
         {
             symbol_num_2--;
         }
-        else if(str1[symbol_num_1] != str2[symbol_num_2])
+        else if(Text->array_of_pointers[num_str1][symbol_num_1] != Text->array_of_pointers[num_str2][symbol_num_2])
         {
-            return (str1[symbol_num_1] - str2[symbol_num_2]);
+            return (Text->array_of_pointers[num_str1][symbol_num_1] - Text->array_of_pointers[num_str2][symbol_num_2]);
         }
         else
         {
@@ -82,7 +87,7 @@ int str_cmp_rhyme(const char* str1, const char* str2)
         symbol_num_2--;
         }
     }
-    return (len_str1 - len_str2);
+    return (Text->len_str[num_str1] - Text->len_str[num_str2]);
 }
 
 int str_len (const char* string)
@@ -106,17 +111,24 @@ void swap (ParsedText* Text, int element_1, int element_2)
     char* point = Text->array_of_pointers[element_1];
     Text->array_of_pointers[element_1] = Text->array_of_pointers[element_2];
     Text->array_of_pointers[element_2] = point;
+
+    int num = Text->len_str[element_1];
+    Text->len_str[element_1] = Text->len_str[element_2];
+    Text->len_str[element_2] = num;
 }
 
-void selection_sort (ParsedText* Text, int (*comp) (const char* str1, const char* str2))
+void selection_sort (ParsedText* Text, int (*comp) (ParsedText* Text, const int num_str1, const int num_str2))
 {
+    assert(Text != NULL);     //TODO assert
+    assert(comp != NULL);
+
     int index_min;
     for (int index = 0; index < Text->length; index++)
     {
         index_min = index;
         for (int num = index; num < Text->length; num ++)
         {
-            if (comp(Text->array_of_pointers[index_min], Text->array_of_pointers[num]) > 0)
+            if (comp(Text, index_min, num) > 0)
             {
                 index_min = num;
             }
--- sorting.h
+++ sorting.h
@@ -1,15 +1,15 @@
 #ifndef SORTING
 #define SORTING
 
-int sor_cmp(const void* str1, const void* str2);
-int sor_cmp_rhyme(const void* str1, const void* str2);
+// int sor_cmp(const void* str1, const void* str2);
+// int sor_cmp_rhyme(const void* str1, const void* str2);
 
-int str_cmp(const char* str1, const char* str2);
-int str_cmp_rhyme(const char* str1, const char* str2);
+int str_cmp(ParsedText* Text, const int num_str1, const int num_str2);
+int str_cmp_rhyme(ParsedText* Text, const int num_str1, const int num_str2);
 
 int str_len (const char* string);
 
 void swap (ParsedText* Text, int element_1, int element_2);
-void selection_sort (ParsedText* Text, int (*cmp) (const char* str1, const char* str2));
+void selection_sort (ParsedText* Text, int (*cmp) (ParsedText* Text, const int num_str1, const int num_str2));
 
 #endif
